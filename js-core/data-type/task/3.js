3512a9fa909bc8c1e0a043f60de9f0e38b829b38
VladRadzivil <vladradziviloff@gmail.com>
2024-01-18 11:50:51 +0100

    feat: add data types notes

---
+++ js-core/data-type/task/3.js
@@ -0,0 +1,9 @@
+// What is the output of the script?
+
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // 1
+
+console.log( `hello ${"name"}` ); // name
+
+console.log( `hello ${name}` ); // Ilya
